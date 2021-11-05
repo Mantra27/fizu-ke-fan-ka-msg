bc8302661285f0bdd7a2b1688ff5fa17407e5267
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-11-05 21:59:37 +0530

    Update alt+f4.h

--- alt+f4.h
+++ alt+f4.h
@@ -6,14 +6,14 @@
 #define to main
 #define Me ()
 #define bombay {
-#define Vishu while
+#define Fizu while
 #define Bhai (
 #define ko 1
 #define milne )
 #define aayaa {
 #define tha cout
 #define lekin <<
-#define vishu "ghar nikal leta hun."
+#define fizu "ghar nikal leta hun."
 #define bhai <<
 #define mile endl;
 #define nahi \
