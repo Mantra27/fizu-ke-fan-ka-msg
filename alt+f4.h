1be46949a2d539b771a2e42691c2baf13684624e
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-11-05 21:45:57 +0530

    Create alt+f4.h

---
+++ alt+f4.h
@@ -0,0 +1,20 @@
+#define oyy using
+#define ahiya namespace
+#define avv std;
+#define aek int
+#define kaam main
+#define kar ()
+#define bro {
+#define tane while
+#define man (
+#define fave 1
+#define tya )
+#define sudhi {
+#define tari cout
+#define fav <<
+#define line "I hate cpp"
+#define print <<
+#define karva endl;
+#define mand \
+    }        \
+    }
