69297c457582afda47f10256450e70aebf637e15
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-11-05 22:04:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
-#include "alt+f4.h"
+#define Kya using
+#define bolti namespace
+#define public std;
+#define wese int
+#define to main
+#define Me ()
+#define bombay {
+#define Fizu while
+#define Bhai (
+#define ko 1
+#define milne )
+#define aayaa {
+#define tha cout
+#define lekin <<
+#define fizu "ghar nikal leta hun."
+#define bhai <<
+#define mile endl;
+#define nahi \
+    }        \
+    }
+#define issliye //
+#define me      //
+#define ab      //
+#define kya     //
+#define karu    //
+
+
 
 Kya bolti public wese to Me bombay
     Fizu Bhai ko milne
