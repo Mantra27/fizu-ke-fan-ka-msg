b81f28e1cc65a756d4cb05deb579622210c6c568
Mantra Gohil <75286433+Mantra27@users.noreply.github.com>
2022-01-12 19:17:45 +0530

    Create zifu.h

---
+++ zifu.h
@@ -0,0 +1,26 @@
+#define Kya using
+#define bolti namespace
+#define public std;
+#define wese int
+#define to main
+#define Me ()
+#define bombay {
+#define Fizu while
+#define Bhai (
+#define ko 1
+#define milne )
+#define aayaa {
+#define tha cout
+#define lekin <<
+#define fizu "ghar nikal leta hun."
+#define bhai <<
+#define mile endl;
+#define nahi \
+    }        \
+    }
+#define issliye //
+#define me      //
+#define ab      //
+#define kya     //
+#define karu    //
+
