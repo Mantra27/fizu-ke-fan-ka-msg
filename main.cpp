09379fbe0f03059e3781627ea8d217a5cad62136
Mantra Gohil <75286433+Mantra27@users.noreply.github.com>
2023-08-03 19:51:46 +0530

    fml

--- main.cpp
+++ main.cpp
@@ -1,31 +1,4 @@
-#include <iostream>
-#define Kya using
-#define bolti namespace
-#define public std;
-#define wese int
-#define to main
-#define Me ()
-#define bombay {
-#define Fizu while
-#define Bhai (
-#define ko 1
-#define milne )
-#define aayaa {
-#define tha cout
-#define lekin <<
-#define fizu "ghar nikal leta hun."
-#define bhai <<
-#define mile endl;
-#define nahi \
-    }        \
-    }
-#define issliye //
-#define me      //
-#define ab      //
-#define kya     //
-#define karu    //
-
-
+#include "zifu"
 
 Kya bolti public wese to Me bombay
     Fizu Bhai ko milne
